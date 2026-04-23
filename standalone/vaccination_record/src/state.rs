6649986c572e304cee4ffa5a6a10d38f50563f41
Ghost <abdulmalikabdulrashid1@gmail.com>
2026-04-23 12:50:48 +0100

    Remove obsolete files and tighten repository structure

--- standalone/vaccination_record/src/state.rs
+++
@@ -1,8 +0,0 @@
-use cw_storage_plus::{Item, Map};
-use cosmwasm_std::Addr;
-use crate::msg::BatchRecord;
-
-pub const ADMIN:           Item<Addr>              = Item::new("admin");
-pub const ISSUER_REGISTRY: Item<Addr>              = Item::new("issuer_registry");
-pub const BATCHES:         Map<&str, BatchRecord>  = Map::new("batches");
-pub const BATCH_COUNT:     Item<u64>               = Item::new("batch_count");
